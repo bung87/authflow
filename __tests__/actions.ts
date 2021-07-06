57b277b376c952b17394c826f1d94f93decd52ce
bung87 <crc32@qq.com>
2021-07-06 18:16:56 +0800

    o

--- __tests__/actions.ts
+++ __tests__/actions.ts
@@ -1,78 +1,77 @@
-
 import AuthFlowActions from './action_types'
-import {Deferred} from "ts-deferred";
-import { AnyAction } from 'redux';
+import { Deferred } from 'ts-deferred'
+import { AnyAction } from 'redux'
 function fetchLoginRequest() {
-    return {
-      type: AuthFlowActions.loginRequest
-    };
+  return {
+    type: AuthFlowActions.loginRequest,
   }
+}
 
-  function fetchLoginFinal() {
-    return {
-      type: AuthFlowActions.loginRequestFinal
-    };
+function fetchLoginFinal() {
+  return {
+    type: AuthFlowActions.loginRequestFinal,
   }
-  
-  function fetchLoginSuccess(playload:any) {
-    return {
-      type: AuthFlowActions.loginRequestSuccess,
-      playload
-    };
-  }
-  
-  function fetchLoginFailure(playload:any) {
-    return {
-      type: AuthFlowActions.loginRequestFailure,
-      playload
-    };
-  }
+}
 
-  function fetchCodeRequest() {
-    return {
-      type: AuthFlowActions.codeRequest
-    };
+function fetchLoginSuccess(playload: any) {
+  return {
+    type: AuthFlowActions.loginRequestSuccess,
+    playload,
   }
+}
 
-  function fetchCodeFinal() {
-    return {
-      type: AuthFlowActions.codeRequestFinal
-    };
+function fetchLoginFailure(playload: any) {
+  return {
+    type: AuthFlowActions.loginRequestFailure,
+    playload,
   }
-  
-  function fetchCodeSuccess(playload:any) {
-    return {
-      type: AuthFlowActions.codeRequestSuccess,
-      playload
-    };
+}
+
+function fetchCodeRequest() {
+  return {
+    type: AuthFlowActions.codeRequest,
   }
-  
-  function fetchCodeFailure(playload:any) {
-    return {
-      type: AuthFlowActions.codeRequestFailure,
-      playload
-    };
+}
+
+function fetchCodeFinal() {
+  return {
+    type: AuthFlowActions.codeRequestFinal,
   }
-  
-  /**
-   * 
-   * @param def Deferred
-   * @returns Thunk function
-   */
-export function fetchLogin<T>(def:Deferred<T>) {
-return (dispatch:(action: AnyAction) => AnyAction,_:()=>any) => {
-    dispatch(fetchLoginRequest());
+}
+
+function fetchCodeSuccess(playload: any) {
+  return {
+    type: AuthFlowActions.codeRequestSuccess,
+    playload,
+  }
+}
+
+function fetchCodeFailure(playload: any) {
+  return {
+    type: AuthFlowActions.codeRequestFailure,
+    playload,
+  }
+}
+
+/**
+ *
+ * @param def Deferred
+ * @returns Thunk function
+ */
+export function fetchLogin<T>(def: Deferred<T>) {
+  return (dispatch: (action: AnyAction) => AnyAction, _: () => any) => {
+    dispatch(fetchLoginRequest())
     return def.promise
-    .then(des => {
-        dispatch(fetchLoginSuccess(des));
+      .then(des => {
+        dispatch(fetchLoginSuccess(des))
         def.resolve(des)
-    })
-    .catch(ex => {
-        dispatch(fetchLoginFailure(ex));
+      })
+      .catch(ex => {
+        dispatch(fetchLoginFailure(ex))
         def.reject(ex)
-    })
-    .finally(() => {
+      })
+      .finally(() => {
         dispatch(fetchLoginFinal())
-    })
-};
+      })
+  }
 }
